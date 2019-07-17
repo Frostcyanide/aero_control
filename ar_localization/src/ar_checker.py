44e907f3badc63c8577119a1ae51bac62c9e4b63
UAV <wheitman9312@gmail.com>
2019-07-17 09:57:45 -0400

    updating

--- ar_localization/src/ar_checker.py
+++ ar_localization/src/ar_checker.py
@@ -10,8 +10,8 @@ from std_msgs.msg import Int32
 
 TARGET_DIS    = 1.0  # target distance away that we want to see the tag
 TARGET_THRESH = 0.1  # error/threshold on either side of target_dis that we will allow
-TAG_ORDER = [26, 39] # the order you need to visit the tags in.
-TAG_COUNT=0
+TAG_ORDER = [77, 76, 42, 74, 75, 40] # the order you need to visit the tags in.
+#TAG_ORDER = [82,83,84]
 
 '''
 You need to fly the drone in position control mode to see the AR tags and 
@@ -36,9 +36,10 @@ class ARDistChecker:
         Also create any class variables you need to store data (such as which 
         tags you have seen already, etc...)
         '''
-        self.ar_pose_sub = rospy.Subscriber("/ar_pose_marker",AlvarMarker,self.ar_pose_cb) 
+        self.ar_pose_sub = rospy.Subscriber("/ar_pose_marker",AlvarMarkers,self.ar_pose_cb) 
         self.ar_tag_seen_pub = rospy.Publisher("/seen_tag",Int32,queue_size=1)
         self.seen_tags=[]
+        self.tag_count=0
         print("Hello")
         #raise Exception("Delete this and fill-in subscriber + publisher initialization!")
         
@@ -58,15 +59,15 @@ class ARDistChecker:
         TODO: figure out what current_marker should be
         '''
         
-        
-        self.current_marker = None
         smallest_dist = 1000
-        
         for marker in msg.markers:
-            if (find_dist(marker) < smallest_dist):
+            if (self.find_dist(marker) < smallest_dist):
                 self.current_marker = marker
-                smallest_dist  = find_dist(marker)
-        self.check_dist()
+                smallest_dist  = self.find_dist(marker)
+        
+        if (self.current_marker is not None):
+            self.check_dist()
+
 
     def find_dist(self, tag):
         return tag.pose.pose.position.z
@@ -83,28 +84,33 @@ class ARDistChecker:
         If new tag is not seen within set distance, tells how far you should go 
         forward to be within range.
         '''
-       ''' seen_marker = self.current_marker
-        if 0.9 < find_dist(self.current_marker) < 1.1:
+       
+        
+        seen_marker = self.current_marker
+        print(seen_marker.id)
+        print("Next one to find: " + str(TAG_ORDER[self.tag_count]))
+        if 0.9 < self.find_dist(self.current_marker) < 1.1:
+            
             if (seen_marker.id in TAG_ORDER):
-                if seen_marker.id==TAG_COUNT:
+                if seen_marker.id==TAG_ORDER[self.tag_count]:
                     #publish
-                    self.ar_tag_seen_pub.publish("ARtag seen: "+seen_marker.id)
-                    TAG_COUNT+=1
-                    self.seen_tags.append(seen_marker)
+                    self.ar_tag_seen_pub.publish((seen_marker.id))
+                    print("*-----*------ARtag seen: "+str(seen_marker.id) +"--------*--------*")
+                    self.tag_count+=1
+                    self.seen_tags.append(seen_marker.id)
 
                 elif seen_marker.id in self.seen_tags:
                     print("Already there")
                 else:
                     print("Come back later")
+
             else:
                 print("Unnecessary Tag")
-        elif find_dist(self.current_marker)<0.9:
-            print("Too close")
-            print("move back "+str(0.9-find_dist(seen_marker))
+        elif self.find_dist(self.current_marker)<0.9:
+            print("move back "+str(0.9-self.find_dist(seen_marker)))
         else:
-            print("Too far")
-            print("move forward "+str(find_dist(seen_marker)-1.1)
-            '''
+            print("move forward "+str(self.find_dist(seen_marker)-1.1))
+            
 
 
         
